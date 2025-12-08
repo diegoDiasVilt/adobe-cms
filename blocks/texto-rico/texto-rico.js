d18849cb0c9968320f7dc5aa599d9693b547b4da
Diego Martins <diego.martins@vilt-group.com>
2025-12-08 15:23:14 -0300

    table texto rico, teste estilização

--- blocks/texto-rico/texto-rico.js
+++ blocks/texto-rico/texto-rico.js
@@ -1,23 +1,34 @@
-import { decodeBase64 } from '../../scripts/scripts.js';
+import { decodeBase64 } from "../../scripts/scripts.js";
+
+function removerStylesInlineDeTabelas() {
+  const seletores = "table, thead, tbody, tfoot, tr, th, td";
+  const elementosParaLimpar = document.querySelectorAll(seletores);
+  // Percorre e remove o atributo 'style'
+  for (const elemento of elementosParaLimpar) {
+    elemento.removeAttribute("style");
+  }
+}
 
 export default function decorate(block) {
   const firstChild = block.children[0];
   const id = block.children[1];
 
+  document.addEventListener("DOMContentLoaded", removerStylesInlineDeTabelas);
+
   if (id) {
-    block.setAttribute('id', id?.textContent?.trim());
+    block.setAttribute("id", id?.textContent?.trim());
   }
 
   if (firstChild) {
-    const paragraph = firstChild.querySelector('p');
+    const paragraph = firstChild.querySelector("p");
     if (paragraph && paragraph.textContent) {
       const decodedHtml = decodeBase64(paragraph.textContent.trim());
       block.innerHTML = decodedHtml;
     } else {
-      block.innerHTML = '';
+      block.innerHTML = "";
       firstChild.remove();
     }
   } else {
-    block.innerHTML = '';
+    block.innerHTML = "";
   }
 }
