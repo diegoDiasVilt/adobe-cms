a1b7be0af1b0d388af26609267cbbe7902b87a88
Diego Martins <diego.martins@vilt-group.com>
2025-12-18 16:58:55 -0300

    insert subtitle field

--- blocks/titulo/titulo.js
+++ blocks/titulo/titulo.js
@@ -1,20 +1,23 @@
 import { decodeBase64 } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const title = block?.children[0];
-  const id = block?.children[1];
-  const icon = block?.children[2];
-  const iconType = block?.children[3];
+  const [ title, subtitle, id, icon, iconType] = block?.children;
 
   const idText = id?.textContent?.trim();
   const iconText = icon?.textContent?.trim();
   const iconTypeText = iconType?.textContent?.trim();
+  const subtitleText = subtitle?.textContent?.trim();
+  
+  if (subtitleText) { 
+    subtitle.classList.add('titulo-subtitle');
+  }
 
   id.remove();
   icon.remove();
   iconType.remove();
 
   const headerElement = title?.querySelector('div:last-child');
+
   if (id) {
     headerElement?.setAttribute('id', idText);
   }
@@ -24,6 +27,7 @@ export default function decorate(block) {
   const sanitizedHeader = headerElement.querySelector('h1,h2,h3,h4,h5,h6');
   headerElement.textContent = '';
   headerElement.append(sanitizedHeader);
+  headerElement.append(subtitle);
 
   if (iconText) {
     const iconElement = document.createElement('i');
