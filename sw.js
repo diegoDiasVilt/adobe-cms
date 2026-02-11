c6f2b5636b1a27baaada2a01e854155769c34a74
jayme.santos@vilt-group.com <jayme.santos@vilt-group.com>
2026-02-11 17:38:26 -0300

    cache control by querystring

--- sw.js
+++ sw.js
@@ -1,36 +1,46 @@
 const CACHE_NAME = 'cogna-router-v1';
 
-self.addEventListener('install', e => {
-    console.log('install')
-    self.skipWaiting(); // Ativa imediatamente
-});
+const IGNORE_DOMAINS = [
+    'aem.page',
+    'aem.live',
+    'localhost',
+];
 
-self.addEventListener('activate', e => {
-    console.log('activate')
-    e.waitUntil(
-        Promise.all([
-            clients.claim(), // Assume controle das abas abertas
-            caches.keys().then(keys => Promise.all(keys.map(key => {
-                if (key !== CACHE_NAME) return caches.delete(key)
-            })))
-        ])
-    );
-});
+self.addEventListener('install', e => self.skipWaiting());
+
+self.addEventListener('activate', e => e.waitUntil(Promise.all([
+    clients.claim(),
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key)
+    })))
+])));
+
+function getCacheKey(url) {
+    const clone = new URL(url)
+    clone.search = '';
+    return clone
+}
 
 self.addEventListener('fetch', async e => {
     const url = new URL(e.request.url);
-
+    const pwa = url.searchParams.get('pwa')
+    switch (pwa) {
+        case 'skip': return;
+        case 'drop': return caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+        case 'refresh': caches.open(CACHE_NAME).then(c => c.delete(getCacheKey(url)))
+    }
+    if (e.request.method !== 'GET' || IGNORE_DOMAINS.some(d => url.hostname.includes(d))) return;
     if (url.pathname.startsWith('/conteudo/')) {
         e.respondWith((async () => {
-            url.search = '';
             const cache = await caches.open(CACHE_NAME);
             console.log('search in cache')
             const cached = await cache.match(e.request, { ignoreSearch: true });
             if (cached) return cached;
 
             console.log('search in net')
-            const res = await fetch(url);
-            if (res.ok) cache.put(url, res.clone());
+            const key = getCacheKey(url)
+            const res = await fetch(key);
+            if (res.ok) cache.put(key, res.clone());
             return res
         })());
     }
