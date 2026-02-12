36494df363b21dd38ff5c9fa543a5c6ffdb9d3dc
jayme.santos@vilt-group.com <jayme.santos@vilt-group.com>
2026-02-12 12:08:02 -0300

    sw improve flow and handle handle errors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cogna-router-v2';
+const CACHE_NAME = 'cogna-pwa-v3';
 
 const IGNORE_DOMAINS = [
     'aem.page',
@@ -6,7 +6,13 @@ const IGNORE_DOMAINS = [
     // 'localhost',
 ];
 
-self.addEventListener('install', e => self.skipWaiting());
+// const OFFLINE_URL = '/offline.html'
+// const PREFETCH = [OFFLINE_URL]
+
+self.addEventListener('install', e => {
+    self.skipWaiting()
+    // e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PREFETCH)))
+});
 
 self.addEventListener('activate', e => e.waitUntil(Promise.all([
     clients.claim(),
@@ -21,27 +27,62 @@ function getCacheKey(url) {
     return clone
 }
 
+function getServiceErrorResponse(event, error) {
+    const nopwa = new URL(event.request.url);
+    nopwa.searchParams.set('pwa', 'skip');
+    const nocache = new URL(event.request.url);
+    nocache.searchParams.set('pwa', 'drop');
+    return new Response(`
+Erro: ${error.message}
+<ul><li><a href="${event.request.url}">Tente Novamente</a></li>
+<li><a href="${nocache}">Limpar Cache</a></li>
+<li><a href="${nopwa}">Desativar PWA</a></li></ul>
+    `, {
+        status: 503,
+        headers: { 'Content-Type': 'text/html' },
+    });
+}
+
 self.addEventListener('fetch', e => {
+    if (e.request.mode !== 'navigate') return;
     const url = new URL(e.request.url);
     const pwa = url.searchParams.get('pwa')
-    switch (pwa) {
-        case 'skip': return;
-        case 'drop': return caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))));
-        case 'refresh': return caches.open(CACHE_NAME).then(c => c.delete(getCacheKey(url)));
-    }
-    if (e.request.method !== 'GET' || IGNORE_DOMAINS.some(d => url.hostname.includes(d))) return;
-    if (url.pathname.startsWith('/conteudo/')) {
-        e.respondWith((async () => {
-            const cache = await caches.open(CACHE_NAME);
-            console.log('search in cache')
-            const cached = await cache.match(e.request, { ignoreSearch: true });
-            if (cached) return cached;
+    try {
+        if (pwa) switch (pwa) {
+            case 'skip': return;
+            case 'drop': e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); return;
+            case 'refresh': e.waitUntil(caches.open(CACHE_NAME).then(c => c.delete(getCacheKey(url))));
+        }
+        if (e.request.method !== 'GET' || IGNORE_DOMAINS.some(d => url.hostname.includes(d))) return;
+        if (url.pathname.startsWith('/conteudo/')) {
+            e.respondWith((async () => {
+                try {
+                    const cache = await caches.open(CACHE_NAME);
+                    console.log('search in cache')
+                    const cached = await cache.match(e.request, { ignoreSearch: true });
+                    if (cached) return cached;
 
-            console.log('search in net')
-            const key = getCacheKey(url)
-            const res = await fetch(key);
-            if (res.ok) cache.put(key, res.clone());
-            return res
-        })());
+                    console.log('search in net')
+                    const key = getCacheKey(url)
+                    const res = await fetch(e.request);
+                    if (res.ok) cache.put(key, res.clone());
+                    return res
+                } catch (error) {
+                    console.error(`Unable to fetch ${url}: ${error.message}`)
+                    // try {
+                    //     const cache = await caches.open(CACHE_NAME)
+                    //     let offline = await cache.match(OFFLINE_URL)
+                    //     if (offline) return offline
+                    //     offline = await fetch(OFFLINE_URL)
+                    //     if (offline.ok) return offline
+                    // } catch (err) {
+                    //     console.error(`Unable to fetch offline page: ${err.message}`)
+                    // }
+                    return getServiceErrorResponse(e, error)
+                }
+            })());
+        }
+    } catch (error) {
+        console.error(error)
     }
 });
