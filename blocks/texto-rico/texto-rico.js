dbbffeeabfae8f467bd368d7630ac3ba4f66672b
lucaslimavilt <lucas.lima@vilt-group.com>
2026-05-12 15:07:24 -0300

    fix: normalize non-breaking spaces in rich text

--- blocks/texto-rico/texto-rico.js
+++ blocks/texto-rico/texto-rico.js
@@ -1,5 +1,20 @@
 import { decodeBase64 } from "../../scripts/scripts.js";
 
+function normalizeNonBreakingSpaces(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+
+  const walker = document.createTreeWalker(template.content, NodeFilter.SHOW_TEXT);
+  let textNode = walker.nextNode();
+
+  while (textNode) {
+    textNode.nodeValue = textNode.nodeValue.replace(/\u00a0/g, " ");
+    textNode = walker.nextNode();
+  }
+
+  return template.innerHTML;
+}
+
 export default function decorate(block) {
   const firstChild = block.children[0];
   const id = block.children[1];
@@ -12,7 +27,7 @@ export default function decorate(block) {
     const paragraph = firstChild.querySelector("p");
     if (paragraph && paragraph.textContent) {
       const decodedHtml = decodeBase64(paragraph.textContent.trim());
-      block.innerHTML = decodedHtml;
+      block.innerHTML = normalizeNonBreakingSpaces(decodedHtml);
     } else {
       block.innerHTML = "";
       firstChild.remove();
