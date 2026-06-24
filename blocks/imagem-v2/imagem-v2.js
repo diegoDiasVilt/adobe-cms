1373e1163dfe36b71a3fd2c9a2319eb36c5aca04
lucaslimavilt <lucas.lima@vilt-group.com>
2026-06-24 14:31:02 -0300

    remove icon and apply zoom in cursor

--- blocks/imagem-v2/imagem-v2.js
+++ blocks/imagem-v2/imagem-v2.js
@@ -52,9 +52,6 @@ export default function decorate(block) {
   if (zoomInVal === 'true') {
     pictureElement?.classList?.add('zoom-in');
 
-    const zoomIcon = document.createElement('i');
-    pictureElement.append(zoomIcon);
-
     const imgElement = image?.querySelector('img');
 
     pictureElement.addEventListener('mousemove', (e) => {
