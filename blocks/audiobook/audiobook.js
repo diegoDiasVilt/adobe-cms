99494eca014d33333ad4a156963d6ca83d0303d8
lucaslimavilt <lucas.lima@vilt-group.com>
2026-08-06 09:17:01 -0300

    update preload

--- blocks/audiobook/audiobook.js
+++ blocks/audiobook/audiobook.js
@@ -31,7 +31,7 @@ export default function decorate(block) {
   const audioEl = document.createElement('audio');
   audioEl.setAttribute('controls', '');
   audioEl.setAttribute('controlsList', 'nodownload noremoteplayback');
-  audioEl.setAttribute('preload', 'none');
+  audioEl.setAttribute('preload', 'auto');
   if (ariaLabelText) audioEl.setAttribute('aria-label', ariaLabelText);
   audioEl.addEventListener('contextmenu', (e) => e.preventDefault());
 
